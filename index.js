2dc01f90c0241ee7359326175878849562de17eb
moad Zizi <moad.zizi@hotmail.com>
2022-09-14 12:54:43 +0200

    adding some HTML for better view :pushpin:

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ fetch("movies.json")
 
         console.log(sortedL);
         sortedL.forEach(element => {
+            document.getElementById("data").innerHTML += 
+            `
+         <div style=" border: 0.5px solid black;">
+            <p style=" margin: 10px;">${element.id}. <br/> ${element.title} <br/> ${element.score/20}/5</p>
+         </div>
+      `
             console.log(element.id + ". " + element.title + " " + element.score / 20 + "/5");
         });
 
