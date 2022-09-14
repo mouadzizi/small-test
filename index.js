15988959b4de40ad71760aa49cb29ee900f29620
moad Zizi <moad.zizi@hotmail.com>
2022-09-14 12:07:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+fetch("movies.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (movies) {
+        var sortedL = movies.data.sort((a, b) => {
+            return (b.score == a.score) ?
+                a.title.localeCompare(b.title)
+                :
+                b.score - a.score
+
+        })
+
+        console.log(sortedL);
+        sortedL.forEach(element => {
+            console.log(element.score + " " + element.title + " " + element.score / 20);
+        });
+
+    });
